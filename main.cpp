d77918bd7cd6c81a7e95ae45863bb7a12bee0a9a
Xawier Slupik <48760619+xBlixon@users.noreply.github.com>
2024-12-25 00:22:33 +0100

    Minor change in the main.cpp

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main() {
     printf("Please enter the equation:\n");
     std::getline(std::cin, strEquation);
     RPNEquation equation = RPNEquation(strEquation);
+    std::cout<<"Result: " << equation.getResult()<<std::endl;
     return 0;
 }
