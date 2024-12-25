48f7a99e2cdbf57b9730b46f60c7cafab4eef71e
Xawier Slupik <48760619+xBlixon@users.noreply.github.com>
2024-12-25 22:53:32 +0100

    Minor refactor

--- lib/RPN.cpp
+++ lib/RPN.cpp
@@ -22,67 +22,71 @@ int calculate(const int& a, const int& b, const std::string& op) {
     }
 }
 
-const std::unordered_set<std::string> RPN::Equation::operators = {
-    "*",
-    "/",
-    "+",
-    "-"
-};
+namespace RPN {
+    const std::unordered_set<std::string> Equation::operators = {
+        "*",
+        "/",
+        "+",
+        "-"
+    };
 
-RPN::Equation::Equation(std::string equation) {
-    this->equation = std::move(equation);
-    this->stream = std::stringstream(this->equation);
-    this->solve();
-}
-
-/**
- * Solves the equation based on
- * the equation member of the struct
- * and assigns the result to the
- * result member of the struct.
- */
-void RPN::Equation::solve() {
-    std::string element;
-    std::stack<int> numbers;
-    while (stream >> element) {
-        std::cout << element << std::endl;
-        if (isOperator(element)) {
-            /**
-             * Takes 2 elements from the stack,
-             * removing the first and reassigning
-             * the second to the result
-             * of the operation.
-             */
-            int b = numbers.top();
-            numbers.pop();
-            int& a = numbers.top();
-            int result = calculate(a, b, element);
-            a = result;
-        } else {
-            numbers.push(std::stoi(element));
-        }
+    Equation::Equation(std::string equation) {
+        this->equation = std::move(equation);
+        this->stream = std::stringstream(this->equation);
+        this->solve();
     }
+
     /**
-     * After the entire algorithm is done
-     * the stack should contain only 1 element,
-     * which is equal to the result of the equation.
+     * Solves the equation based on
+     * the equation member of the struct
+     * and assigns the result to the
+     * result member of the struct.
      */
-    result = numbers.top();
+    void Equation::solve() {
+        std::string element;
+        std::stack<int> numbers;
+        while (stream >> element) {
+            std::cout << element << std::endl;
+            if (isOperator(element)) {
+                /**
+                 * Takes 2 elements from the stack,
+                 * removing the first and reassigning
+                 * the second to the result
+                 * of the operation.
+                 */
+                int b = numbers.top();
+                numbers.pop();
+                int& a = numbers.top();
+                int result = calculate(a, b, element);
+                a = result;
+            } else {
+                numbers.push(std::stoi(element));
+            }
+        }
+        /**
+         * After the entire algorithm is done
+         * the stack should contain only 1 element,
+         * which is equal to the result of the equation.
+         */
+        result = numbers.top();
+    }
+
+    /**
+     * Returns the result of the given equation.
+     */
+    double Equation::getResult() const {
+        return result;
+    }
+
+    /**
+     * Checks if the string is a mathematical operator
+     * supported by this struct.
+     * @param op String tested for being a valid math operator
+     * @return
+     */
+    bool Equation::isOperator(const std::string& op) {
+        return operators.count(op) > 0;
+    }
 }
 
-/**
- * Returns the result of the given equation.
- */
-double RPN::Equation::getResult() const {
-    return result;
-}
 
-/**
- * Checks if the string is a mathematical operator
- * supported by this struct.
- * @param op String tested for being a valid math operator
- * @return
- */
-bool RPN::Equation::isOperator(const std::string& op) {
-    return operators.count(op) > 0;
-}
